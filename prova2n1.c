e0aa681c7cd542ab37045b8bdb3dbc6f147f553c
Lefpe <147464434+Lefpe@users.noreply.github.com>
2023-10-27 18:11:49 -0300

    Update prova2n1.c

--- prova2n1.c
+++ prova2n1.c
@@ -161,6 +161,64 @@ void listaAlfabetica(struct Catalogo *catalogo, int ordem) {
     }
 }
 //fim da função
+//listar por ano//
+void informarquantidade (struct Catalogo *catalogo, int ano){
+  struct Livros *atual = catalogo->head;
+  int quantidade = 0;
+  while (atual != NULL) {
+    if (atual->ano == ano) {
+      quantidade++;
+      
+    }
+    else {
+      printf("não há livros nesta data");
+    }
+  }
+}
+
+//fim da função//
+//nome de autor por ano //
+void listarnomeautoraposano (struct Catalogo *catalogo, char autor[60], int ano){
+  struct Livros *atual = catalogo->head;
+  int livroEncontrado = 0; // Variável para verificar se há livros do autor na data especificada
+  
+  while (atual != NULL) {
+    if (atual->ano == ano) {
+      if (strcmp(atual->autor, autor) == 0) {
+        printf("%s\n", atual->titutlo);
+        livroEncontrado = 1; // Configura a variável para indicar que há pelo menos um livro do autor na data especificada
+      }
+    }
+    atual = atual->tale;
+  }
+  
+  if (!livroEncontrado) {
+    printf("Não há livros do autor %s nesta data.\n", autor);
+  }
+}
+
+//fim da função//
+
+//remover livros por ano//
+void removerporano(struct Catalogo *catalogo, int ano) {
+  struct Livros *atual = catalogo->head;
+  struct Livros *prev = NULL;
+  while (atual != NULL) {
+    if (atual->ano == ano) {
+      if (prev == NULL) {
+        catalogo->head = atual->tale;
+      } else {
+        prev->tale = atual->tale;
+      }
+      free(atual);
+      atual = prev != NULL ? prev->tale : catalogo->head;
+    } else {
+      prev = atual;
+      atual = atual->tale;
+    }
+  }
+}
+//fim da função//
 
 
 int main(void) {
@@ -174,14 +232,16 @@ int main(void) {
     int busca;
 //optei por usar um menu desta forma para que a lista ficasse em loop, pois usando somente switch, a lista fechava ao inserir um valor, não armazenando e não conseguindo acessar alguns ponteiros//
     while (1) {
-      printf("\n-- Catálogo --\n");
-      printf("Digite a opção que você deseja:\n");
-      printf("1. Inserir Livros\n");
-      printf("2. Deletar Livros\n");
-      printf("3. Buscar Livro\n");
-      printf("4. Exibir Catálogo\n");
-      printf ("5.ordenar lista \n");
-      printf("6. Sair\n");
+    printf("\n-- Catálogo --\n");
+    printf("Digite a opção que você deseja:\n");
+    printf("1. Inserir Livros\n");
+    printf("2. Deletar Livros\n");
+    printf("3. Buscar Livro\n");
+    printf("4. Exibir Catálogo\n");
+    printf("5. listar em ordem alfabética");
+    printf("6. Sair\n");
+    printf("8.deletar livros por ano");
+    printf ("7.informar livros por ano");
 
       scanf("%d", &options);
       switch (options){
@@ -209,64 +269,88 @@ int main(void) {
 
       case 2:
           printf("-- Deletar Livro --\n");
-          char isbn_del[14];
-          printf("Digite o ISBN do livro a ser removido: ");
-          scanf("%s", isbn_del);
+      char isbn_del[14];
+      printf("Digite o ISBN do livro a ser removido: ");
+      scanf("%s", isbn_del);
 
-          printf("Exibindo dados do livro:\n");
-          buscarLivros(catalogo, isbn_del);
+      printf("Exibindo dados do livro:\n");
+      buscarLivros(catalogo, isbn_del);
 
-          printf("Deseja remover exemplares deste livro? (1-Sim / 2-Não): ");
-          scanf("%d", &del);
+      printf("Deseja remover exemplares deste livro? (1-Sim / 2-Não): ");
+      scanf("%d", &del);
 
-          if (del == 1) {
-              int nremover;
-              printf("Quantidade de exemplares a remover: ");
-              scanf("%d", &nremover);
-              deletarLivros(catalogo, isbn_del, nremover);
-          } else if (del == 2) {
-              printf("Operação de remoção cancelada.\n");
-          } else {
-              printf("Opção inválida.\n");
-          }
+      if (del == 1) {
+        int nremover;
+        printf("Quantidade de exemplares a remover: ");
+        scanf("%d", &nremover);
+        deletarLivros(catalogo, isbn_del, nremover);
+      } else if (del == 2) {
+        printf("Operação de remoção cancelada.\n");
+      } else {
+        printf("Opção inválida.\n");
+      }
           break;
 
       case 3:
           printf("-- Buscar Livro --\n");
-          printf("Selecione a forma de busca:\n");
-          printf("1. Por ISBN\n");
-          printf("2. Por Autor\n");
-          printf("Qualquer outra tecla para sair\n");
-          scanf("%d", &busca);
+      printf("Selecione a forma de busca:\n");
+      printf("1. Por ISBN\n");
+      printf("2. Por Autor\n");
+      printf("Qualquer outra tecla para sair\n");
+      scanf("%d", &busca);
 
-          if (busca == 1) {
-              char isbn_search[14];
-              printf("Digite o ISBN do livro: ");
-              scanf("%s", isbn_search);
-              printf("Exibindo dados do livro:\n");
-              buscarLivros(catalogo, isbn_search);
-          } else if (busca == 2) {
-              char autor_search[60];
-              printf("Digite o nome do autor: ");
-              scanf(" %[^\n]", autor_search);
-              printf("Exibindo dados dos livros do autor:\n");
-              // Código para buscar e exibir livros por autor
-              struct Livros *aux = catalogo->head;
-              while (aux != NULL) {
-                  if (strcmp(aux->autor, autor_search) == 0) {
-                      printf("Nome do livro: %s\n", aux->titutlo);
-                      printf("ISBN: %s\n", aux->isbn);
-                      printf("Número de exemplares: %d\n", aux->numexemplares);
-                      printf("Ano de publicação: %d\n", aux->ano);
-                      printf("Autor: %s\n", aux->autor);
-                      printf("===============================\n");
-                  }
-                  aux = aux->tale;
-              }
-          } else {
-              printf("Opção inválida.\n");
-          }
-          break;
+      if (busca == 1) {
+        char isbn_search[14];
+        printf("Digite o ISBN do livro: ");
+        scanf("%s", isbn_search);
+        printf("Exibindo dados do livro:\n");
+        buscarLivros(catalogo, isbn_search);
+      } else if (busca == 2) {
+        char autor_search[60];
+        printf("Digite o nome do autor: ");
+        scanf(" %[^\n]", autor_search);
+        printf("Deseja buscar após determinado ano? (1 - Sim / 2 - Não): ");
+        scanf("%d", &buscaporano);
+
+        if (buscaporano == 1) {
+          printf("Digite o ano inicial de publicação dos livros a serem "
+                 "pesquisados: ");
+          scanf("%d", &lo);
+          printf("Exibindo dados dos livros do autor %s a partir do ano %d:\n",
+                 autor_search, lo);
+          listarnomeautoraposano(catalogo, autor_search, lo);
+        } else if (buscaporano == 2) {
+          printf("Exibindo dados dos livros do autor %s:\n", autor_search);
+          listarnomeautoraposano(catalogo, autor_search, 0);
+        } else {
+          printf("Opção inválida.\n");
+        }
+      }
+    }
+    if (buscaporano == 2) {
+
+    } else {
+      printf("Exibindo dados dos livros do autor %s:\n", autor_search);
+    }
+    printf("Exibindo dados dos livros do autor:\n");
+    // Código para buscar e exibir livros por autor
+    struct Livros *aux = catalogo->head;
+    while (aux != NULL) {
+      if (strcmp(aux->autor, autor_search) == 0) {
+        printf("Nome do livro: %s\n", aux->titutlo);
+        printf("ISBN: %s\n", aux->isbn);
+        printf("Número de exemplares: %d\n", aux->numexemplares);
+        printf("Ano de publicação: %d\n", aux->ano);
+        printf("Autor: %s\n", aux->autor);
+        printf("===============================\n");
+      }
+      aux = aux->tale;
+    }
+  }
+  else {
+    printf("Opção inválida.\n");
+  }
+  break;
 
       case 4:
           printf("-- Exibir Catálogo --\n");
@@ -292,6 +376,39 @@ int main(void) {
          free(catalogo);
                 exit(0);
         break;
+        case 7:
+           void informarQuantidadeLivrosPorAno(struct Catalogo *catalogo, int anoInicio, int anoFim) {
+  struct Livros *atual = catalogo->head;
+  int quantidade = 0;
+  while (atual != NULL) {
+    if (atual->ano >= anoInicio && atual->ano <= anoFim) {
+      quantidade++;
+    }
+    atual = atual->tale;
+  }
+  printf("Quantidade de livros publicados entre %d e %d: %d\n", anoInicio, anoFim, quantidade);
+}
+   
+        break;
+      case 8:
+        //deletar livros por ano //
+  printf("Digite o ano inicial de publicação dos livros a serem pesquisados: ");
+  scanf("%d", &lo);
+  printf("Deseja buscar após determinado ano? (1 - Sim / 2 - Não): ");
+  scanf("%d", &buscaporano);
+  
+  if (buscaporano == 1) {
+    int anoDeletar;
+    printf("Digite o ano inicial de publicação dos livros a serem deletados: ");
+    scanf("%d", &anoDeletar);
+
+    // Código para deletar livros por ano
+    removerporano(catalogo, anoDeletar);
+      
+  } else if (buscaporano == 2) {
+    informarquantidade(catalogo, lo);
+  }
+      break;
 
             default:
                 printf("Opção inválida. Tente novamente.\n");
